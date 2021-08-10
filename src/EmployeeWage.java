586aba10d0c1e84f320897501f0e7b593a5d43d7
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-10 12:44:38 +0530

    employee daily wage calculated

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -4,11 +4,18 @@ public class EmployeeWage {
 	public static void main(String[] args) 
 	{
 		int is_Present=1;
+		int Emp_Rate_Per_Hour=20;
+		int empHour=0;
+		int empWage=0;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if(is_Present==empCheck)
-			System.out.println("Employee is present");
+		   empHour=8;
 		else
-			System.out.println("Emplyoee is Absent");
+		    empHour=0;
+		
+		empWage=empHour*Emp_Rate_Per_Hour;
+		System.out.println("Employee Daily Wage = "+empWage);
+		   
 	}
 
 }
