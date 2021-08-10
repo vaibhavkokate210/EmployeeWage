bbdca05de3d2b101a068ec164cc8fff91436936e
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-10 13:10:46 +0530

    total monthly wages done

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -4,25 +4,33 @@ public class EmployeeWage {
 	public static final int is_Part_Time=2;
 	public static final int is_Full_Time=1;
 	public static final int Emp_Rate_Per_Hour=20;
+	public static final int No_Of_Working_Days=20;
 	
 	public static void main(String[] args) 
 	{
 		int empHour=0;
 		int empWage=0;
-		int empCheck=(int)(Math.floor(Math.random()*10)%3);
-		switch(empCheck)
-		{
-		case is_Full_Time:empHour=8;
-		                  break;
-		                  
-		case is_Part_Time:empHour=4;
-                          break;
-        
-		default:empHour=0;
-		}
+		int totalWage=0;
 		
-		empWage=empHour*Emp_Rate_Per_Hour;
-		System.out.println("Employee Daily Wage = "+empWage);
+		for(int i=0;i<No_Of_Working_Days;i++)
+		{
+		      int empCheck=(int)(Math.floor(Math.random()*10)%3);
+		
+		      switch(empCheck)
+		      {
+		            case is_Full_Time:empHour=8;
+		                              break;
+		                  
+		            case is_Part_Time:empHour=4;
+                                      break;
+        
+		            default:empHour=0;
+		      }
+		
+		     empWage=empHour*Emp_Rate_Per_Hour;
+		     totalWage=totalWage+empWage;
+		}
+		System.out.println("Employee Total Wage = "+totalWage);
 		   
 	}
 
