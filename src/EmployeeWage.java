efb153d01e8858f73cd338acbd20b1b02da12064
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-10 12:52:13 +0530

    employee part time wages added

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -3,13 +3,16 @@ public class EmployeeWage {
 
 	public static void main(String[] args) 
 	{
-		int is_Present=1;
+		int is_Part_Time=2;
+		int is_Full_Time=1;
 		int Emp_Rate_Per_Hour=20;
 		int empHour=0;
 		int empWage=0;
-		double empCheck=Math.floor(Math.random()*10)%2;
-		if(is_Present==empCheck)
+		double empCheck=Math.floor(Math.random()*10)%3;
+		if(is_Full_Time==empCheck)
 		   empHour=8;
+		else if(is_Part_Time==empCheck)
+			empHour=4;
 		else
 		    empHour=0;
 		
