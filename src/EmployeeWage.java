cbef7f5c3f171fb030e0ff88957d894764cfea9f
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-10 12:33:35 +0530

    employee is checked

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,9 +1,14 @@
 
 public class EmployeeWage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		int is_Present=1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(is_Present==empCheck)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Emplyoee is Absent");
 	}
 
 }
